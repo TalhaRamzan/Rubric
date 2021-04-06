424e7f3dc5c425145b469941c9be69552d38646d
TalhaRamzan <82025022+TalhaRamzan@users.noreply.github.com>
2021-04-06 14:54:04 +0500

    Add files via upload

---
+++ src/rubric/Rubric.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubric;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Rubric {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Form1 f =new Form1();
+        f.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
